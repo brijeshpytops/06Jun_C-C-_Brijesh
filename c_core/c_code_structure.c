7a75559ce7e41c363000590ab9a82e25550f675b
chintan <chintan.tops@gmail.com>
2024-06-13 15:41:52 +0530

    structure of c, operators theory

---
+++ c_core/c_code_structure.c
@@ -0,0 +1,13 @@
+// s-1] documetation section
+// add two numbers
+// s-2] linking section
+#include<stdio.h>
+// s-3] global declaration section [optional]
+// s-4] function declaration section [optional]
+// s-5] main function
+void main(){
+    // variable declaration
+    int a = 10, b = 20, c;
+    c = a + b;
+    printf("%d", c);
+}
