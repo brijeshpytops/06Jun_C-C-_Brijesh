c24221b62d6d7275b828677a6124ebec47cfef8c
chintan <chintan.tops@gmail.com>
2024-06-15 14:44:19 +0530

    first_code is done

---
+++ c_core/first_code_in_c.c
@@ -0,0 +1,14 @@
+// Display This Information using printf
+// a. Your Name
+// b. Your Birth date
+// c. Your Age
+// d. Your Address
+
+#include<stdio.h>
+
+void main(){
+    printf("My name is: Aansi\n");
+    printf("My birth-date is: 21 march, 2007\n");
+    printf("My age is: 17\n");
+    printf("My address is: A-1104 apple harmony near maharaja farm mota varaccha surat");
+}
