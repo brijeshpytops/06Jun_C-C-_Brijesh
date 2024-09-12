581a72af705a81441f99316516f58edfe082f10d
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-09-12 14:07:17 +0530

    string

---
+++ C-core/c_core/strings/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+    //char name[] = "tops";
+    // char first_name[7] = { 'b', 'r', 'i', 'j', 'e', 's', 'h'};
+
+    // printf("%c", name[1]);
+    // printf("%s", name);
+    // printf("%s", first_name);
+}
